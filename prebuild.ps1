fc11e45d7b07fb1981194ad1ad60698921fcd4c6
Tony Richards <trichards57@users.noreply.github.com>
2021-02-12 17:24:22 +0000

    Updated
    * Set up to support netstandard 2.0
    * Updated to remove the .ToArray from the paragraph generation to improving performance for large outputs.
    * Updated to match Microsoft open-source best practice.
    
    Breaking Change: Anything returning an IEnumerable<T> can no-longer be relied on to be stable over multiple iterations.  If this is behaviour you need, convert it to a List or array first.

--- prebuild.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Patching Project version..."
-(Get-Content .\Lorem.NET\Lorem.NET.csproj).replace('<Version>2.0.0</Version>', "<Version>$($Env:APPVEYOR_BUILD_VERSION)</Version>") | Set-Content .\Lorem.NET\Lorem.NET.csproj
-(Get-Content .\Lorem.NET\Lorem.NET.csproj).replace('<AssemblyVersion>2.0.0</AssemblyVersion>', "<AssemblyVersion>$($Env:APPVEYOR_BUILD_VERSION)</AssemblyVersion>") | Set-Content .\Lorem.NET\Lorem.NET.csproj
-(Get-Content .\Lorem.NET\Lorem.NET.csproj).replace('<FileVersion>2.0.0</FileVersion>', "<FileVersion>$($Env:APPVEYOR_BUILD_VERSION)</FileVersion>") | Set-Content .\Lorem.NET\Lorem.NET.csproj
-(Get-Content .\Lorem.NET\Lorem.NET.csproj).replace('$commitMsg', $Env:APPVEYOR_REPO_COMMIT_MESSAGE) | Set-Content .\Lorem.NET\Lorem.NET.csproj
-Write-Host "Updated Project to use version $($Env:APPVEYOR_BUILD_VERSION)"
