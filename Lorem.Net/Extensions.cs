cc7a869fc3ebc4729bb1dcc5142b582029d46b92
Tony Richards <trichards57@pm.me>
2025-07-23 18:46:56 +0100

    Removed support for .Net Framework and unsupported .Net versions.

--- Lorem.Net/Extensions.cs
+++ Lorem.Net/Extensions.cs
@@ -7,78 +7,43 @@
 
 using System;
 
-namespace LoremNET
+namespace LoremNET;
+
+/// <summary>
+/// Class containing extension methods to support string manipulation.
+/// </summary>
+internal static class Extensions
 {
     /// <summary>
-    /// Class containing extension methods to support string manipulation.
+    /// Removes the specified pattern.
     /// </summary>
-    internal static class Extensions
+    /// <param name="s">The string to remove <paramref name="pattern"/> from.</param>
+    /// <param name="pattern">The pattern to remove.</param>
+    /// <returns>A new string with all instances of <paramref name="pattern" /> removed.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="s"/> and <paramref name="pattern"/> must not be null.</exception>
+    internal static string Remove(this string s, string pattern)
     {
-        /// <summary>
-        /// Removes the specified pattern.
-        /// </summary>
-        /// <param name="s">The string to remove <paramref name="pattern"/> from.</param>
-        /// <param name="pattern">The pattern to remove.</param>
-        /// <returns>A new string with all instances of <paramref name="pattern" /> removed.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="s"/> and <paramref name="pattern"/> must not be null.</exception>
-        internal static string Remove(this string s, string pattern)
+        ArgumentNullException.ThrowIfNull(s);
+        ArgumentNullException.ThrowIfNull(pattern);
+
+        return s.Replace(pattern, string.Empty);
+    }
+
+    /// <summary>
+    /// Capitalises the first letter.
+    /// </summary>
+    /// <param name="s">The string to capitalise.</param>
+    /// <returns>A new string with the first character capitalised.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="s"/> must not be null.</exception>
+    internal static string UppercaseFirst(this string s)
+    {
+        // Check for empty string.
+        if (string.IsNullOrEmpty(s))
         {
-            if (s == null)
-            {
-                throw new ArgumentNullException(nameof(s), "the string must not be null");
-            }
-
-            if (pattern == null)
-            {
-                throw new ArgumentNullException(nameof(pattern), "the pattern to replace must not be null");
-            }
-
-            return s.Replace(pattern, string.Empty);
+            return string.Empty;
         }
 
-        /// <summary>
-        /// Splits the string using the specified separator.
-        /// </summary>
-        /// <param name="s">The string to split.</param>
-        /// <param name="separator">The separator.</param>
-        /// <returns><paramref name="s" /> split into an array of strings.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="s"/> and <paramref name="separator"/> must not be null.</exception>
-        internal static string[] Split(this string s, string separator)
-        {
-            if (s == null)
-            {
-                throw new ArgumentNullException(nameof(s), "the string must not be null");
-            }
-
-            if (separator == null)
-            {
-                throw new ArgumentNullException(nameof(separator), "the separator must not be null.");
-            }
-
-            return s.Split(new[] { separator }, StringSplitOptions.None);
-        }
-
-        /// <summary>
-        /// Capitalises the first letter.
-        /// </summary>
-        /// <param name="s">The string to capitalise.</param>
-        /// <returns>A new string with the first character capitalised.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="s"/> must not be null.</exception>
-        internal static string UppercaseFirst(this string s)
-        {
-            if (s == null)
-            {
-                throw new ArgumentNullException(nameof(s), "the string must not be null");
-            }
-
-            // Check for empty string.
-            if (string.IsNullOrEmpty(s))
-            {
-                return string.Empty;
-            }
-
-            // Return char and concat substring.
-            return char.ToUpper(s[0]) + s.Substring(1);
-        }
+        // Return char and concat substring.
+        return char.ToUpperInvariant(s[0]) + s[1..];
     }
 }
